7fb49f83f4f88e9610e719083578366fac09e872
JuliaGodlewskaWNE <j.godlewsk11@student.uw.edu.pl>
2025-02-27 04:27:00 +0100

    Add files via upload

--- Zaj3_zad3.R
+++ Zaj3_zad3.R
@@ -9,6 +9,7 @@ library(qdap)
 frequent_terms <- freq_terms(text)
 frequent_terms 
 frequent_terms <- freq_terms(text, stopwords = Top200Words) 
+frequent_terms
 plot(frequent_terms)
 
 # Tworzenie chmury słów za pomocą pakietu wordcloud 
@@ -48,3 +49,69 @@ wordcloud(frequent_terms$WORD, frequent_terms$FREQ, max.words = 5, colors = brew
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Blues"))
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Reds"))
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Greens"))
+
+#Analiza tekstów
+
+#Tekst 2021
+#Top 10 słów
+#1  american
+#2  jobs
+#3  plan
+#4  americans
+#5  act
+#6  families
+#7  lets
+#8  thats
+#9  democracy
+#10 president
+
+#Tekst 2024
+#Top 10 słów
+#1  president
+#2  american
+#3  im
+#4  thats
+#5  future
+#6  years
+#7  americans
+#8  pay
+#9  ive
+#10 tax
+
+#Dalsza optymalizacja
+#Bez słów "zapychaczy"
+
+frequent_terms <- freq_terms(text, stopwords = c(Top200Words, "thats", "im", "ive"))
+frequent_terms
+
+#Dalsza analiza
+
+#Tekst 2021
+#Top 10 słów
+#1  american
+#2  jobs
+#3  plan
+#4  americans
+#5  act
+#6  families
+#7  lets
+#8  democracy
+#9  president
+#10 care
+
+#Tekst 2024
+#Top 10 słów
+#1  president
+#2  american
+#3  future
+#4  years
+#5  americans
+#6  pay
+#7  tax
+#8  dont
+#9  history
+#10 tonight
+
+#Widać pewne różnice w priorytetach. 
+#W pierwszym tekście większe skupienie na słowach o charakterze społecznym/socjalnym (jobs, families, care).
+#W drugim na słowach o charakterze typowo politycznym.
